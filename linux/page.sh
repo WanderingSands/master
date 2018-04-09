b90a396aeedde9423abe660a759efe44205e1bbf
Jonathan Henry <jonathan.henry1987@gmail.com>
2018-04-09 03:05:36 +0000

    modified page.sh to request the current and ending page for said book and removed pagenum.txt as when the user is in another directory, the script would fail.

--- linux/page.sh
+++ linux/page.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-file="~/master/linux/pagenum.txt"
-number=$(cat $"pagenum.txt")
+#file="~/master/linux/pagenum.txt"
+#number=$(cat $"pagenum.txt")
 echo "What page are you on?"
 read pagenum
-
+echo "How many readable pages are there in the book you are going though?"
+read number
 echo "You have $(($number-$pagenum)) pages to go."
